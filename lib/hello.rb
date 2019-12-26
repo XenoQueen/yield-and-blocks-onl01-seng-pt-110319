64c4fd77382640f9d43829f6e309c5e0effd0816
Maika Kailani <queenmab051@gmail.com>
2019-12-26 16:23:10 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def hello_t(array)
   if block_given?
     i = 0
@@ -8,5 +9,24 @@ def hello_t(array)
     array
   else
     puts "Hey! No block was given!"
+=======
+def hello_t(name)
+name.each do |name|
+  puts "Hi, #{name}."
+  end
+end
+
+#def hello_t(names)
+#names = []
+#names.each do |names|
+ # puts #{names}
+ # end
+#names("Tim", "Tom", "Jim")
+#end
+
+["Tim", "Tom", "Jim"].each do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+>>>>>>> ed94d54df629c4730bd1abcd9d4cb5e099e80a44
   end
 end
